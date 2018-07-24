c904326333f01213ef7663ae462cab6e7fd197a9
gtech <gtech@macbook-test-04.local>
2018-07-24 15:55:06 +0800

    Change Pic

--- shoebil/ViewController.swift
+++ shoebil/ViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var shoebill: UIImageView!
+    @IBAction func growSegControl(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 { //判斷使用者選擇是哪一個Segmented 0是出生
+          shoebill.image = UIImage(named: "0")
+        } else if sender.selectedSegmentIndex == 1 {
+            shoebill.image = UIImage(named: "1")
+            
+        } else {
+            shoebill.image = UIImage(named: "2")
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
